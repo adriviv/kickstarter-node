8caba91bc8e393bd2dd469e4297bec953f00cd38
Adrien Viviani <adrien.viv@gmail.com>
2019-05-30 18:22:12 +0800

    mail newsletter ok

--- controllers/newsletterController.js
+++ controllers/newsletterController.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose'); // have access to database
 const Newsletter = mongoose.model('Newsletter');
+const mail = require('../handlers/mail')
 
 exports.subscribe = async (req, res) => {
     const newsletter = new Newsletter(req.body);
     await newsletter.save();
-    // res.json({ status: 'created'});
+    const user = await Newsletter.findOne({email: req.body.email});
+    await mail.send({
+        user: user,
+        subject: 'Your Inscription to the newsletter',
+        filename: '_newsletterRegister',
+        category: 'newsletter',
+    });
     res.send("has been created");
 };
 
---
+++ handlers/mail.js
@@ -0,0 +1,58 @@
+const nodemailer = require('nodemailer'); // package to send email 
+const pug = require('pug');
+const juice = require('juice'); //allow style text like css 
+const htmlToText = require('html-to-text'); // to allow email to receive HTML like <strong>
+const promisify = require('es6-promisify');
+
+
+// a transport is a way to manage to send email
+const transport = nodemailer.createTransport({
+    host: process.env.MAIL_HOST, // all the infos are variables.env
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+});
+
+// Just to test y-you do the following
+// transport.sendMail({
+//     from: 'Adrien <adrien@gmail.com>',
+//     to: 'yolo@example.com',
+//     subject: 'just try it is working',
+//     html: 'hey I <strong>love</strong> you',
+//     text: 'Hey I **love you**'
+// });
+
+// // TO test you export in start.js comme suit and check your inbox
+// // !!!!!!! delete then 
+// // ==> 
+// require('./handlers/mail');
+
+
+
+// TO RENDER THE HTML FILE IN VIEWS/CATGEORY/FILENAME
+const generateHTML = (filename, options = {}) => {
+    const html = pug.renderFile(`${__dirname}/../views/${options.category}/${filename}.pug`,
+     options);
+     const inlined = juice(html); // active the css style
+     return inlined;
+};
+
+
+// LA FORMULE POUR ENVOYER LE MAIL 
+exports.send = async (options) => {
+    const html = generateHTML(options.filename, options); // reference à la ligne 31 -32
+    const text =  htmlToText.fromString(html); // To allow the email to receive HTML like <strong>
+    const mailOptions = {
+    from: 'Adrien <adrien@gmail.com>',
+    to: options.user.email,
+    subject: options.subject,
+    html: html, // possibility to write only html, 
+    text: text, 
+    };
+    const sendMail = promisify(transport.sendMail, transport);
+    return sendMail(mailOptions);
+};
+
+// VOIR le mail pug to see how to interpolate the dat in the mail
