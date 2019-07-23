fd2837eadd9987dd495325269df949e05c4a87ef
adriviv <46681102+adriviv@users.noreply.github.com>
2019-07-23 19:08:24 +0200

    Update Review.js

--- models/Review.js
+++ models/Review.js
@@ -27,10 +27,6 @@ const reviewSchema = new mongoose.Schema({
     }
 });
 
-// EACH OF AUTHOR INFORMATION WILL BE ACCESSIBLE thanks to that
-//=======================================================
-// when the author is queried , it automaticcaly autopopulate for us that we need information abot the author
-// c'est pour transformer le author qui est 5362C3jhc4444YY4 en username, avatar etc .. 
 function autopopulate(next) {
     this.populate('author');
     next();
@@ -38,9 +34,5 @@ function autopopulate(next) {
 
 reviewSchema.pre('find', autopopulate);
 reviewSchema.pre('findOne', autopopulate);
-//=======================================================
-
-
-// Then go to sart.js to require it. 
 
 module.exports = mongoose.model('Review', reviewSchema);
