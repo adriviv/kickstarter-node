5316de0b90bac93ca7f77c2fec1f80b1dae423af
adriviv <46681102+adriviv@users.noreply.github.com>
2019-07-23 19:06:19 +0200

    Update Pledge.js

--- models/Pledge.js
+++ models/Pledge.js
@@ -24,14 +24,6 @@ const pledgeSchema = new mongoose.Schema({
 });
 
 
-
-
-
-
-// EACH OF AUTHOR INFORMATION WILL BE ACCESSIBLE thanks to that
-//=======================================================
-// when the author is queried , it automaticcaly autopopulate for us that we need information abot the author
-// c'est pour transformer le author qui est 5362C3jhc4444YY4 en username, avatar etc .. 
 function autopopulate(next) {
     this.populate('author');
     next();
@@ -39,10 +31,5 @@ function autopopulate(next) {
 
 pledgeSchema.pre('find', autopopulate);
 pledgeSchema.pre('findOne', autopopulate);
-//=======================================================
-
-
-
-// Then go to sart.js to require it. 
 
 module.exports = mongoose.model('Pledge', pledgeSchema);
