165fe8950a85b8396e38ecf31bf8078a1aba9ee5
adriviv <46681102+adriviv@users.noreply.github.com>
2019-07-23 19:02:46 +0200

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose'); // have access to database
+const mongoose = require('mongoose'); 
 const Review = mongoose.model('Review');
 
 
 //                              ADD 
 // -------------------------------------------------------
 exports.addReview = async (req, res) => {
-    // res.json(req.body); 
-    // // when console.log See only text and rating. We want all params from the model so we add them mannualy 
-    req.body.author = req.user._id; // save this review in the user 
-    req.body.project = req.params.id; // save this review in the store
-    // res.json(req.body); 
+    req.body.author = req.user._id; 
+    req.body.project = req.params.id; 
     const newReview = new Review(req.body);
     await newReview.save();
     res.send("has been created");
@@ -20,8 +17,3 @@ exports.getReviews = async (req, res) => {
  res.json({reviews: reviews});
 
 };
-// exports.getProjects = async (req, res) => {
-//     // res.json({ it: 'Worked'})
-//     const projects = await Project.find();
-//     res.json({projects: projects});
-// };
